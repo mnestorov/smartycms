16ae1f1f20c2adff069bb2f0665365c00647d279
Martin Nestorov <mbnestorov@gmail.com>
2021-10-21 10:34:23 +0300

    PHPUnit test package update and bug fixing for tests.

--- tests/unit/ConsoleTest.php
+++ tests/unit/ConsoleTest.php
@@ -6,13 +6,18 @@ use SmartyStudio\SmartyCms\Tests\SmartyCmsTestCase;
 
 class ConsoleTest extends SmartyCmsTestCase
 {
-	public function tearDown()
+	public function tearDown() : void
 	{
 		parent::tearDown();
 	}
 
 	public function testInstall()
 	{
-		$this->assertTrue(0 < $this->artisan('smartycms:install', ['prefix' => 'administration', 'admin' => 'admin', 'email' => 'admin@example.com', 'password' => '123']));
+		$this->assertTrue(0 < $this->artisan('smartycms:install', [
+            'prefix'    => 'admin',
+            'admin'     => 'admin',
+            'email'     => 'admin@example.com',
+            'password'  => '123'
+        ]));
 	}
 }
